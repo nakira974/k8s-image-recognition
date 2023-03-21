fd96f3892ca7030f3c880461817fea87a73e6588
nakira974 <maxime.loukhal@gmail.com>
2023-03-21 22:59:37 +0100

    Add a text generation model for image embeddings

--- unsplash/create_model.py
+++ unsplash/create_model.py
@@ -1,13 +1,41 @@
 import pandas as pd
 import glob
 from PIL import Image
+from google.auth.transport import requests
 
 from tensorflow.lite.python.conversion_metadata_schema_py_generated import np
 
 import tensorflow as tf
 from keras.applications.inception_v3 import preprocess_input
 from keras import Model
-from keras.layers import Input, Flatten, Dense, Dropout
+from keras.layers import Input, Flatten, Dense, Dropout, LSTM, RepeatVector, TimeDistributed
+from huggingface_hub import login
+from transformers import AutoTokenizer, AutoModel, AutoConfig
+
+
+def create_text_generation_model():
+    embedding_dim = 128
+    num_decoder_tokens = 28
+    latent_dim = 256
+
+    # Define the input layer for the decoder
+    input_decoder_layer = Input(shape=(None, num_decoder_tokens))
+
+    # Define the LSTM decoder
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(input_decoder_layer)
+
+    # Add a dense layer
+    decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    # Define the output layer for the decoder
+    output_decoder_layer = decoder_outputs
+
+    # Create the model
+    result_text_generation_model = Model(inputs=[input_decoder_layer], outputs=[output_decoder_layer])
+
+    return result_text_generation_model
 
 
 def create_embedding_model():
@@ -50,8 +78,9 @@ def create_embeddings(dataset, p_embedding_model):
     color_hex_codes = dataset['colors']['hex']
     batch_size = 32
     result_embeddings = []
+    login(token="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")
 
-
+    # Define the model and tokenizer
     for i in range(0, len(photo_ids), batch_size):
         # Load a batch of images
         batch_ids = photo_ids[i:i + batch_size]
@@ -73,6 +102,25 @@ def create_embeddings(dataset, p_embedding_model):
     return result_embeddings
 
 
+def prepare_data_for_text_generation(embedding_matrix, dataframe):
+    photo_ids = dataframe.photo_id
+    descriptions = []
+    for i in range(len(photo_ids)):
+        description = "<start> " + dataframe.iloc[i]['description'] + " <end>"
+        descriptions.append(description)
+
+    # Tokenize the textual descriptions
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    sequences = tokenizer(descriptions, padding='max_length', truncation=True, max_length=64, return_tensors='tf')
+
+    # Create input and output sequences for training
+    encoder_input_data = embedding_matrix
+    decoder_input_data = sequences.input_ids
+    decoder_output_data = sequences.attention_mask
+
+    return encoder_input_data, decoder_input_data, decoder_output_data
+
+
 path = './unsplash_datasets/'
 documents = ['photos', 'keywords', 'collections', 'conversions', 'colors']
 datasets = {}
@@ -91,3 +139,11 @@ print(datasets)
 embedding_model = create_embedding_model()
 embeddings = create_embeddings(datasets, embedding_model)
 print(embeddings)
+
+text_generation_model = create_text_generation_model()
+encoder_input_data, decoder_input_data, decoder_target_data = prepare_data_for_text_generation(embeddings,
+                                                                                               datasets['photos'])
+text_generation_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+text_generation_model.fit([decoder_input_data], [decoder_target_data], epochs=10, batch_size=32)
+text_generation_model.save('wickr-bot.keras')
