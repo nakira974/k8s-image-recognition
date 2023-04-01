93cdfca8d928f1791cf39f0793ec55081ad133db
nakira974 <maxime.loukhal@gmail.com>
2023-04-01 21:48:00 +0200

    - Add fastapi to expose pix2struct on /model/descrivizio-001

--- unsplash/main.py
+++ unsplash/main.py
@@ -1,26 +1,50 @@
-import os
-
-from unsplash.SoucoupeV1 import Soucoupe
-from huggingface_hub import login
-from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
-import requests
+from typing import Any, Dict
+import io
 from PIL import Image
-from decouple import config
 import torch
+from fastapi import FastAPI, HTTPException
+from fastapi.param_functions import Body
+from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
+from decouple import config
+from huggingface_hub import login
+from fastapi.logger import logger as fastapi_logger
+from logging.handlers import RotatingFileHandler
+import logging
+import uvicorn
 
-print("CUDA DEVICE :"+torch.__version__)
-print("IS CUDA AVAILABLE : ", end=torch.cuda.is_available().__str__()+"\n")
-
+app = FastAPI()
 API_KEY = config('HUGGING_FACE_TOKEN')
-
 login(API_KEY)
-
 google_model = Pix2StructForConditionalGeneration.from_pretrained("Aloblock/descrivizio")
 processor = Pix2StructProcessor.from_pretrained("Aloblock/descrivizio")
 
-url = "https://www.ilankelman.org/stopsigns/australia.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-inputs = processor(images=image, return_tensors="pt")
-predictions = google_model.generate(**inputs)
-print(processor.decode(predictions[0], skip_special_tokens=True))
+uvicorn.run(app, host="0.0.0.0", port=7777, log_config=f"./log.ini")
+
+class ImagePredictionController:
+    def __init__(self, model: Pix2StructForConditionalGeneration, processor: Pix2StructProcessor) -> None:
+        self.model = model
+        self.processor = processor
+
+    async def predict(self, image_bytes: bytes) -> Dict[str, Any]:
+        try:
+            print("Processing image...")
+            image = Image.open(io.BytesIO(image_bytes))
+            inputs = self.processor(images=image, return_tensors="pt")
+            predictions = self.model.generate(**inputs)
+            prediction_text = self.processor.decode(predictions[0], skip_special_tokens=True)
+            print("Image process ended...")
+            return {"prediction": prediction_text}
+        except:
+            raise HTTPException(status_code=400, detail="Error processing image")
+
+
+controller = ImagePredictionController(google_model, processor)
+
+
+@app.post('/model/descrivizio-001')
+async def predict_image(image_bytes: bytes = Body(...)):
+    return await controller.predict(image_bytes)
+
+
+
 
