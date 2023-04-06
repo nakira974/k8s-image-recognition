3e4741adeb1a19a1959f4f94e66ffda58b9ed7ce
nakira974 <maxime.loukhal@gmail.com>
2023-04-06 22:24:44 +0200

    Add route table associations and nat for routing and ping

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -82,6 +82,52 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "private" {
+  count = length(data.aws_availability_zones.available.names)
+
+  vpc_id     = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.${count.index + 1}.128/25"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "k8s-private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.private[0].id
+
+  depends_on = [
+    aws_internet_gateway.k8s_igw,
+    aws_subnet.private,
+    aws_route_table_association.private,
+  ]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "k8s-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private.*.id)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_security_group" "k8s_node_sg" {
   name_prefix = "k8s-node-"
   vpc_id = aws_vpc.k8s_vpc.id
