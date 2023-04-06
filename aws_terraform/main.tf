e14b9562e65de85c986196d341500c3b9ec827ae
nakira974 <maxime.loukhal@gmail.com>
2023-04-06 18:24:42 +0200

    conf deploy ok

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -3,78 +3,67 @@ provider "aws" {
   profile = "nakira974"
 }
 
+
+resource "aws_key_pair" "nakira974-ssh" {
+  key_name   = "nakira974-ssh"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "k8s_node" {
   ami           = "ami-0f960c8194f5d8df5"
   instance_type = "t3.medium"
   count         = 3
-
+  subnet_id     = aws_subnet.public[count.index].id
+  key_name= "nakira974-ssh"
   tags = {
     Name = "k8s-node-${count.index}"
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    port        = 22
-    private_key = "${file("~/.ssh/id_rsa")}"
-    host        = self.public_ip
-    agent       = false
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get -y upgrade",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
-      "sudo touch /etc/apt/sources.list.d/kubernetes.list",
-      "echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list",
-      "sudo apt-get update",
-      "sudo apt-get install -y kubelet kubeadm kubectl",
-    ]
-  }
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo apt-get update
+            sudo apt-get -y upgrade
+            sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+            curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+            sudo touch /etc/apt/sources.list.d/kubernetes.list
+            echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+            sudo apt-get update
+            sudo apt-get install -y kubelet kubeadm kubectl
+             EOF
 }
 
 resource "aws_instance" "k8s_master" {
   ami           = "ami-0f960c8194f5d8df5"
   instance_type = "t3.medium"
-
+  key_name= "nakira974-ssh"
+  subnet_id     = aws_subnet.public[0].id
   tags = {
     Name = "k8s-master"
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    port        = 22
-    private_key = "${file("~/.ssh/id_rsa")}"
-    host        = self.public_ip
-    agent       = false
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get -y upgrade",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
-      "sudo touch /etc/apt/sources.list.d/kubernetes.list",
-      "echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list",
-      "sudo apt-get update",
-      "sudo apt-get install -y kubelet kubeadm kubectl",
-      "sudo kubeadm init --pod-network-cidr=192.168.0.0/16 > /tmp/k8s-init.log",
-      "sudo mkdir -p /home/ubuntu/.kube",
-      "sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config",
-      "sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config",
-      "kubectl taint nodes --all node-role.kubernetes.io/master-",
-      "kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml",
-      "kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml",
-      "kubectl create serviceaccount dashboard-admin-sa -n kubernetes-dashboard",
-      "kubectl create clusterrolebinding dashboard-admin-sa --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:dashboard-admin-sa",
-      "kubectl describe secrets -n kubernetes-dashboard $(kubectl get secrets -n kubernetes-dashboard | grep dashboard-admin-sa | awk '{print $1}') > /tmp/dashboard-admin-token.txt",
-    ]
-  }
-
+  user_data = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get -y upgrade
+      sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+      curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+      sudo touch /etc/apt/sources.list.d/kubernetes.list
+      echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+      sudo apt-get update
+      sudo apt-get install -y kubelet kubeadm kubectl
+      sudo kubeadm init --pod-network-cidr=192.168.0.0/16 > /tmp/k8s-init.log
+      sudo mkdir -p /home/ubuntu/.kube
+      sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+      sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config",
+      kubectl taint nodes --all node-role.kubernetes.io/master-
+      kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
+      kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+      kubectl create serviceaccount dashboard-admin-sa -n kubernetes-dashboard
+      kubectl create clusterrolebinding dashboard-admin-sa --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:dashboard-admin-sa
+      kubectl describe secrets -n kubernetes-dashboard $(kubectl get secrets -n kubernetes-dashboard | grep dashboard-admin-sa | awk '{print $1}') > /tmp/dashboard-admin-token.txt
+        EOF
 }
 
 
@@ -82,7 +71,6 @@ resource "aws_lb" "k8s_lb" {
   name               = "k8s-lb"
   internal           = false
   load_balancer_type = "application"
-
   subnets = aws_subnet.public.*.id
 
   security_groups = [
@@ -117,7 +105,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_security_group" "k8s_node_sg" {
   name_prefix = "k8s-node-"
-
+  vpc_id = aws_vpc.k8s_vpc.id
   ingress {
     from_port= 0
     to_port   = 65535
@@ -135,7 +123,7 @@ resource "aws_security_group" "k8s_node_sg" {
 
 resource "aws_security_group" "k8s_lb_sg" {
   name_prefix = "k8s-lb-"
-
+  vpc_id = aws_vpc.k8s_vpc.id
   ingress {
     from_port = 80
     to_port   = 80
@@ -189,28 +177,25 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public.*.id)
-
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_network_interface_attachment" "k8s_master_eni" {
   instance_id = aws_instance.k8s_master.id
-  device_index = 0
+  device_index = 1
   network_interface_id = aws_network_interface.k8s_master_eni.id
 }
 
 resource "aws_network_interface_attachment" "k8s_node_eni" {
   for_each = { for idx, instance in aws_instance.k8s_node : idx => instance }
-
   instance_id         = each.value.id
-  device_index        = 0
+  device_index        = 1
   network_interface_id = aws_network_interface.k8s_node_eni[each.key].id
 }
 
 resource "aws_network_interface" "k8s_node_eni" {
   for_each = { for idx, subnet in aws_subnet.public : idx => subnet }
-
   subnet_id        = each.value.id
   security_groups  = [aws_security_group.k8s_node_sg.id]
   tags             = { Name = "k8s-node-eni-${each.key + 1}" }
@@ -218,7 +203,6 @@ resource "aws_network_interface" "k8s_node_eni" {
 
 resource "aws_network_interface" "k8s_master_eni" {
   subnet_id = aws_subnet.public[0].id
-
   security_groups = [
     aws_security_group.k8s_node_sg.id,
   ]
@@ -246,7 +230,6 @@ resource "aws_lb_listener" "k8s_listener" {
   load_balancer_arn = aws_lb.k8s_lb.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     target_group_arn = aws_lb_target_group.k8s_tg.arn
     type             = "forward"
@@ -258,15 +241,6 @@ resource "aws_lb_listener" "k8s_listener" {
   ]
 }
 
-resource "null_resource" "kubeconfig" {
-  provisioner "local-exec" {
-    command = "rm -rf ~/.kube && mkdir -p ~/.kube && scp -o StrictHostKeyChecking=no ubuntu@${aws_instance.k8s_master.public_ip}:/home/ubuntu/.kube/config ~/.kube"
-  }
-
-  depends_on = [
-    aws_instance.k8s_master,
-  ]
-}
 
 output "k8s_cluster_url" {
   value = "https://${aws_lb.k8s_lb.dns_name}"
