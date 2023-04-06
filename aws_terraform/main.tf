6b6c589299532a0444277fa6f4cd5878418a5c3d
nakira974 <maxime.loukhal@gmail.com>
2023-04-06 20:01:26 +0200

    add ssh ingress into k8s_lb_sg

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -108,6 +108,14 @@ resource "aws_security_group" "k8s_lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port = 443
     to_port   = 443
