9c78e2b3a8a38928cd26586f59b14c9604924e84
nakira974 <maxime.loukhal@gmail.com>
2023-04-12 20:52:12 +0200

    Add icmp to ingress

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -99,6 +99,13 @@ resource "aws_security_group" "k8s_node_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 65535
@@ -122,6 +129,13 @@ resource "aws_security_group" "k8s_lb_sg" {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
