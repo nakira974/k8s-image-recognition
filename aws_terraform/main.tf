41769ef72894c09747703135f61b675a62bc8ed7
nakira974 <maxime.loukhal@gmail.com>
2023-04-12 19:48:52 +0200

    update ssh ingress

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_key_pair" "nakira974-ssh" {
 }
 
 resource "aws_instance" "k8s_node" {
-  ami           = "ami-0f960c8194f5d8df5"
+  ami           = "ami-064087b8d355e9051"
   instance_type = "t3.medium"
   associate_public_ip_address = true
   count         = 3
@@ -32,7 +32,7 @@ resource "aws_instance" "k8s_node" {
 }
 
 resource "aws_instance" "k8s_master" {
-  ami           = "ami-0f960c8194f5d8df5"
+  ami           = "ami-064087b8d355e9051"
   associate_public_ip_address = true
   instance_type = "t3.medium"
   key_name= "nakira974-ssh"
@@ -131,6 +131,13 @@ resource "aws_security_group" "k8s_node_sg" {
     to_port   = 65535
     protocol  = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
