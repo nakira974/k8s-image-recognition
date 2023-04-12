863719f24bcf101525b3741db042cd5ea5511e64
nakira974 <maxime.loukhal@gmail.com>
2023-04-12 21:14:45 +0200

    removed public route association

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -255,11 +255,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  count = length(aws_subnet.public.*.id)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
 
 
 resource "aws_network_interface_attachment" "k8s_node_eni" {
