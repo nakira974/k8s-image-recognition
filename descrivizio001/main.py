e5df59e47629bb50755c049cd44beccf64b81b93
nakira974 <maxime.loukhal@gmail.com>
2023-04-15 23:40:45 +0200

    Update Dockerfile for descrivizio001 and removed uvicorn launch in main.py

--- descrivizio001/main.py
+++ descrivizio001/main.py
@@ -1,16 +1,8 @@
-from typing import Any, Dict
-import io
-from PIL import Image
 from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.param_functions import Body
 from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
 from decouple import config
 from huggingface_hub import login
-from pydantic import BaseModel
 from ImagePredictionController import ImagePredictionController
-from ApplicationImage import ApplicationImage
-import uvicorn
-import base64
 
 app = FastAPI()
 API_KEY = config('HUGGING_FACE_TOKEN')
@@ -24,6 +16,4 @@ controller = ImagePredictionController(google_model, processor)
 async def predict_image(request: Request):
     return await controller.predict(request)
 
-
-uvicorn.run(app, host="0.0.0.0", port=7777, log_config=f"./log.ini")
-
+# uvicorn.run(app, host="0.0.0.0", port=7777, log_config=f"./log.ini")
